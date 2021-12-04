c38b6a38be5b226b838042b2db48583ff3eac22c
Marc Backes <hello@marc.dev>
2021-12-04 20:19:30 +0100

    initial commit 🍬

---
+++ src/utils/candy.js
@@ -0,0 +1,12 @@
+export const generateCandyBags = (bagCount, candyCount) => {
+    // Return an array of "candy bags".
+    // Each bag has a unique ID - (UUID)
+    // Each bag has `candies` - an array of candy
+    // -   When no parameter is passed, return an empty array
+    // -   When the `candyCount` parameter is not passed, default to 3
+    // -   Each generated bag should have a valid UUID
+    // -   No candy should be in the bag more than once
+    // -   Not every bag should have the same candy (mix it up!)
+    // -   If `candyCount` is higher than
+    return []
+}
